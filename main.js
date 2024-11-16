e9710738a35b5276295d4a039dce4ee0a1d1abfe
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-16 16:28:59 +0530

    remove word from a file

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('fs')
+const readline = require('readline')
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-});
+  output: process.stdout,
+})
 
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
     // TODO: Implement this function
-    rl.close();
-  });
-});
+
+    fs.readFile(filename, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(`Error reading file: ${err.message}`)
+        rl.close()
+        return
+      }
+
+      const words = data.split(' ')
+      const filteredWords = words?.filter((item) => item !== word)
+      const updatedContent = words.join(' ')
+
+      fs.writeFile(filename, updatedContent, (err) => {
+        if (err) {
+          console.error(`Error writing to file: ${err.message}`)
+        } else {
+          console.log(`The word "${word}" has been removed from the file.`)
+        }
+        rl.close()
+      })
+    })
+
+    rl.close()
+  })
+})
